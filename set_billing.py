c6dbcf042fc259d7f711d088e0f25734bae91374
peeya-i <35793565+peeya-i@users.noreply.github.com>
2020-04-11 23:39:48 -0700

    Remove imports not used in the function

--- set_billing.py
+++ set_billing.py
@@ -1,7 +1,5 @@
 def set_billing(bac_id, project_id):
     from googleapiclient import discovery
-    from subprocess import PIPE, Popen
-    import os
 
     name = 'projects/' + project_id 
     service = discovery.build('cloudbilling', 'v1',  cache_discovery=False)
